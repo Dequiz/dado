e029b36e6853e0dee35038eff5d993ac42915f70
Dequiz <128110472+Dequiz@users.noreply.github.com>
2024-02-29 01:55:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,31 @@ function rolarDado(){
     document.getElementById("resultado").innerHTML = resultado.toString();
 
     if (resultado === 1) {
-        document.getElementById("imagem").innerHTML = "<img src='dado1.png'>";
+        document.getElementById("dado").innerHTML = "<img src='dado1.png'>";
     } else if (resultado === 2) {
-        document.getElementById("imagem").innerHTML = "<img src='dado2.png'>";
+        document.getElementById("dado").innerHTML = "<img src='dado2.png'>";
     } else if (resultado === 3) {
-        document.getElementById("imagem").innerHTML = "<img src='dado3.png'>";
+        document.getElementById("dado").innerHTML = "<img src='dado3.png'>";
     } else if (resultado === 4) {
-        document.getElementById("imagem").innerHTML = "<img src='dado4.png'>";
+        document.getElementById("dado").innerHTML = "<img src='dado4.png'>";
     } else if (resultado === 5) {
-        document.getElementById("imagem").innerHTML = "<img src='dado5.png'>";
+        document.getElementById("dado").innerHTML = "<img src='dado5.png'>";
     } else if (resultado === 6) {
-        document.getElementById("imagem").innerHTML = "<img src='dado6.png'>";
+        document.getElementById("dado").innerHTML = "<img src='dado6.png'>";
     }
 }
+function pararRotacao() {
+    // Seleciona o elemento com a classe 'rotating-image'
+    var dadoElement = document.querySelector('img');
+    
+    // Remove a classe 'rotating-image' para interromper a animação
+    dadoElement.classList.remove('img');
+    
+}
+
+
+    
+
 
 
 
